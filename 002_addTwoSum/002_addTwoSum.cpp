895809aae8d0b5e1a0977508eaf9553c7bb0f9e5
comoco-5672 <775324623@qq.com>
2023-06-01 23:47:09 +0800

    添加2

---
+++ 002_addTwoSum/002_addTwoSum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *addTwoNum(ListNode *l1, ListNode *l2) {
+    ListNode *dummy = new ListNode();
+    int carry = 0;
+    ListNode *cur = dummy;
+    while (l1 || l2 || carry) {
+      int s = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+      carry = s / 10;
+      cur->next = new ListNode(s % 10);
+      cur = cur->next;
+      l1 = l1 ? l1->next : nullptr;
+      l2 = l2 ? l2->next : nullptr;
+    }
+    return dummy->next;
+  }
+};
+
+int main()
+{
+  std::vector<int> l1 = {2, 4, 3}, l2 = {5, 6, 4};
+  Solution solu;
+  solu.addTwoNum(l1, l2);
+  return 0;
+}
