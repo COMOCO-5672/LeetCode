970ce12ff3ed0e2b6e9f0c22d51f03877730c4fe
“jiaopl” <jiaopl@bokecc.com>
2023-06-28 12:57:44 +0800

    添加注释

--- 002_addTwoSum/002_addTwoSum.cpp
+++ 002_addTwoSum/002_addTwoSum.cpp
@@ -16,8 +16,9 @@ public:
     int carry = 0;
     ListNode *cur = dummy;
     while (l1 || l2 || carry) {
+      // 如果
       int s = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
-      carry = s / 10;
+      carry = s / 10; // 取整数
       cur->next = new ListNode(s % 10);
       cur = cur->next;
       l1 = l1 ? l1->next : nullptr;
