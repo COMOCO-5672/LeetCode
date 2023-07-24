8e2f0f40f5ed3aa22d046ba320e0e90ceef97b18
comoco-5672 <775324623@qq.com>
2023-07-25 00:09:59 +0800

    添加第11题

---
+++ online/11.盛最多水的容器.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=cpp
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+#include <vector>
+
+class Solution {
+public:
+  int maxArea(std::vector<int> &height) {
+    int len = height.size() - 1;
+    if (len < 0)
+      return 0;
+  }
+};
+// @lc code=end
