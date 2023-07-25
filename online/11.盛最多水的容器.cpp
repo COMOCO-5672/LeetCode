8fa91d41c4dbe53ae0b51eb00245ef1b21ff7e1a
comoco-5672 <775324623@qq.com>
2023-07-25 21:32:00 +0800

    添加第11题

--- online/11.盛最多水的容器.cpp
+++ online/11.盛最多水的容器.cpp
@@ -10,9 +10,26 @@
 class Solution {
 public:
   int maxArea(std::vector<int> &height) {
-    int len = height.size() - 1;
-    if (len < 0)
+    if (height.size() < 1)
       return 0;
+
+    int start_idx = 0;
+    int end_idx = height.size() - 1;
+
+    int maxCap = 0;
+    int cacheCap = 0;
+
+    while (start_idx < end_idx) {
+      cacheCap =
+          (end_idx - start_idx) * min(height[start_idx], height[end_idx]);
+      maxCap = max(maxCap, cacheCap);
+      if (height[start_idx] < height[end_idx]) {
+        start_idx++;
+      } else {
+        end_idx--;
+      }
+    }
+    return maxCap;
   }
 };
 // @lc code=end
