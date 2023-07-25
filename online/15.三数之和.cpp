28544281b339bcb11a60371c662ccc923d16f4b0
comoco-5672 <775324623@qq.com>
2023-07-25 22:35:39 +0800

    完成第15题

--- online/15.三数之和.cpp
+++ online/15.三数之和.cpp
@@ -18,8 +18,26 @@ public:
       return ans;
     sort(nums.begin(), nums.end());
 
-    for (int i = 0; i < nsize && nums[i] == 0; i++) {
-      if (i && nums[i - 1] == nums[i]) {
+    for (int i = 0; i < nsize && nums[i] <= 0; i++) {
+      if (i && nums[i - 1] == nums[i])
+        continue;
+      int l = i + 1, r = nums.size() - 1;
+      int target = 0 - nums[i];
+      while (l < r) {
+        if (nums[l] + nums[r] < target)
+          l++;
+        else if (nums[l] + nums[r] > target)
+          r--;
+        else {
+          std::vector<int> ians = {nums[i], nums[l], nums[r]};
+          ans.push_back(ians);
+          l++;
+          r--;
+          while (l < r && nums[l - 1] == nums[l])
+            l++;
+          while (l < r && nums[r + 1] == nums[r])
+            r--;
+        }
       }
     }
 
