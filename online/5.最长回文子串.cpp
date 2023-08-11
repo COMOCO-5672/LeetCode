763a775128e7e46020e8b5779f5033900f5f8c95
comoco-5672 <775324623@qq.com>
2023-08-11 22:19:33 +0800

    添加文件

--- online/5.最长回文子串.cpp
+++ online/5.最长回文子串.cpp
@@ -9,41 +9,58 @@
 #include <string>
 #include <vector>
 
-int dp[1010][1010];
+//int dp[1010][1010];
 
 class Solution {
 public:
   std::string longestPalindrome(std::string s) {
-    int len = s.size();
-    if (len == 0 || len == 1)
-      return s;
-    int start = 0; // 回文串起始位置
-    int max = 1;   // 回文串最大长度
-    std::vector<std::vector<int>> dp(len,
-                                     std::vector<int>(len)); // 定义二维动态数组
-    for (int i = 0; i < len; i++)                            // 初始化状态
+    // int len = s.size();
+    // if (len == 0 || len == 1)
+    //   return s;
+    // int start = 0; // 回文串起始位置
+    // int max = 1;   // 回文串最大长度
+    // std::vector<std::vector<int>> dp(len,
+    //                                  std::vector<int>(len)); // 定义二维动态数组
+    // for (int i = 0; i < len; i++)                            // 初始化状态
+    // {
+    //   dp[i][i] = 1;
+    //   if (i < len - 1 && s[i] == s[i + 1]) {
+    //     dp[i][i + 1] = 1;
+    //     max = 2;
+    //     start = i;
+    //   }
+    // }
+    // for (int l = 3; l <= len;
+    //      l++) // l表示检索的子串长度，等于3表示先检索长度为3的子串
+    // {
+    //   for (int i = 0; i + l - 1 < len; i++) {
+    //     int j = l + i - 1;                         // 终止字符位置
+    //     if (s[i] == s[j] && dp[i + 1][j - 1] == 1) // 状态转移
+    //     {
+    //       dp[i][j] = 1;
+    //       start = i;
+    //       max = l;
+    //     }
+    //   }
+    // }
+    // return s.substr(start, max); // 获取最长回文子串
+
+    int n = s.size(), ans = 0;
+    std::vector<std::vector<bool>> dp(n, std::vector<bool>(n));
+
+    for (int i = 0; i < n; i++)
     {
-      dp[i][i] = 1;
-      if (i < len - 1 && s[i] == s[i + 1]) {
-        dp[i][i + 1] = 1;
-        max = 2;
-        start = i;
-      }
-    }
-    for (int l = 3; l <= len;
-         l++) // l表示检索的子串长度，等于3表示先检索长度为3的子串
-    {
-      for (int i = 0; i + l - 1 < len; i++) {
-        int j = l + i - 1;                         // 终止字符位置
-        if (s[i] == s[j] && dp[i + 1][j - 1] == 1) // 状态转移
+      for (int j = i; j>=0; j--)
+      {
+        dp[j][i] = (s[i] == s[j] && (i-j <= 2 || dp[j+1][i-1]));
+        if()
         {
-          dp[i][j] = 1;
-          start = i;
-          max = l;
+          
         }
       }
     }
-    return s.substr(start, max); // 获取最长回文子串
+    
+
   }
 };
 // @lc code=end
