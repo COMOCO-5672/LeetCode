9fae7c46215b1c9c66ad4fe137d3550bd0cc5dd0
comoco-5672 <775324623@qq.com>
2023-10-22 09:45:43 +0800

    添加剪枝操作

--- online/77. 组合.cpp
+++ online/77. 组合.cpp
@@ -17,11 +17,17 @@ public:
         if(path.size() == k)
         {
             path_ret.emplace_back(path);
+            for (int i = 0; i< path.size(); ++i)
+            {
+                std::cout << path[i] << ",";
+            }
+            std::cout << std::endl;
             return;
         }
 
-        for (int i = startIndex; i <= n; i++)
+        for (int i = startIndex; i <= n - ( k - path.size()) + 1; i++)
         {
+            std::cout << "i:" << i << std::endl;
             path.emplace_back(i);
             backTracling(n,k,i + 1);
             path.pop_back();
@@ -46,6 +52,10 @@ Solution::~Solution()
 
 int main()
 {
+
+    Solution sol;
+    sol.combine(4,4);
+
     return 1;
 }
 
