997aaaea84cf0a50f2d4847bd441a5171972d585
comoco-5672 <775324623@qq.com>
2023-10-31 22:05:46 +0800

    添加选择排序

---
+++ WriteMemory1/2.选择排序.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+void selectedsort(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                std::swap(a[i], a[j]);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int a[] = {2,4,6,8,0,1,3,5,7,9};
+    selectedsort(a, 10);
+
+    for (int k = 0; k < 10; k++) {
+        std::cout << a[k] << "  ";
+    }
+
+    return 0;
+}
+
