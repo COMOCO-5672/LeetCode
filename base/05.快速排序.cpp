9ba165bf784b6094f82a4912791ecce9bd917d21
“jiaopl” <jiaopl@bokecc.com>
2023-11-01 12:38:30 +0800

    默写快排算法

--- base/05.快速排序.cpp
+++ base/05.快速排序.cpp
@@ -28,7 +28,7 @@ void quicksort(std::vector<int> &nums, int l, int r) {
 
 int main() {
   std::vector<int> a = {0, 34, 66, 2, 5, 95, 4, 46, 27};
-  quicksort(a, 0, sizeof(a) / sizeof(int));
+  quicksort(a, 0, a.size());
   for (int i = 0; i <= 8; ++i) {
     std::cout << a[i] << " "; // print => 0 2 4 5 27 34 46 66 95
   }
