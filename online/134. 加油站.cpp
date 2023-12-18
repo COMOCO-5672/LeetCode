8df5d47894d55cadfac472019c428b647a869921
comoco-5672 <775324623@qq.com>
2023-12-18 22:37:51 +0800

    格式化

--- online/134. 加油站.cpp
+++ online/134. 加油站.cpp
@@ -5,23 +5,23 @@ using namespace std;
 
 int canComplateCircuit(std::vector<int> &gas, std::vector<int> &cost) {
   int totalGas = 0;
-    int currentGas = 0;
-    int startStation = 0;
+  int currentGas = 0;
+  int startStation = 0;
 
-    for (int i = 0; i < gas.size(); ++i) {
-        totalGas += gas[i] - cost[i];
-        currentGas += gas[i] - cost[i];
+  for (int i = 0; i < gas.size(); ++i) {
+    totalGas += gas[i] - cost[i];
+    currentGas += gas[i] - cost[i];
 
-        // 如果当前油量小于0，说明无法从当前加油站出发
-        // 重置起始加油站，并重置当前油量
-        if (currentGas < 0) {
-            startStation = i + 1;
-            currentGas = 0;
-        }
+    // 如果当前油量小于0，说明无法从当前加油站出发
+    // 重置起始加油站，并重置当前油量
+    if (currentGas < 0) {
+      startStation = i + 1;
+      currentGas = 0;
     }
+  }
 
-    // 如果总油量小于0，说明无法绕一圈
-    return (totalGas >= 0) ? startStation : -1;
+  // 如果总油量小于0，说明无法绕一圈
+  return (totalGas >= 0) ? startStation : -1;
 }
 
 int main() {
