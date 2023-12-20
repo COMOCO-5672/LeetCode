8a2f2ec9e6226436b263da070e23952dc7d7576c
comoco-5672 <775324623@qq.com>
2023-12-20 23:02:02 +0800

    添加N字变换

---
+++ online/6. N 字形变换.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::string convert(std::string s, int numRows) {
+  if (numRows == 1 || numRows >= s.size()) {
+    return s;
+  }
+
+  std::vector<std::string> rows(numRows);
+
+  int curRow = 0;
+  bool goingDown = false;
+
+  for (char c : s) {
+    rows[curRow] += c;
+    if (curRow == 0 || curRow == numRows - 1) {
+      goingDown = !goingDown;
+    }
+    curRow += goingDown ? 1 : -1;
+  }
+
+  std::string result;
+  for (const std::string &row : rows) {
+    result += row;
+  }
+
+  return result;
+}
+
+int main() {
+  string input = "PAYPALISHIRING";
+  int numRows = 3;
+  string result = convert(input, numRows);
+
+  cout << "Converted string: " << result << endl;
+  return 0;
+}
