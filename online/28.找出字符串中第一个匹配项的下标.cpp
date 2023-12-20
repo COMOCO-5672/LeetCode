1283d8c74afff6d4de340fab4fb693a30ee812e8
comoco-5672 <775324623@qq.com>
2023-12-20 21:47:48 +0800

    完成匹配第一个字符串的下标

--- online/28.找出字符串中第一个匹配项的下标.cpp
+++ online/28.找出字符串中第一个匹配项的下标.cpp
@@ -5,11 +5,45 @@
  */
 
 // @lc code=start
+
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
+  int strStr(string haystack, string needle) {
+    int len1 = haystack.size(), len2 = needle.size();
 
+    if (len2 < 1 || len1 < 1) {
+      return -1;
     }
+
+    int l1 = 0, l2 = 0, match = 0, index = 0;
+    int sign = -1;
+    while (l1 < len1 && l2 < len2) {
+      if (haystack[l1] == needle[l2]) {
+        if (haystack[l1] = needle[0] && sign == -1) {
+          index = l1;
+          sign = 1;
+        }
+
+        ++match;
+        ++l1;
+        ++l2;
+        continue;
+      }
+      l2 = 0;
+      sign = -1;
+      if (match > 0) {
+        l1 = l1 - match + 1;
+      } else {
+        ++l1;
+      }
+      match = 0;
+      index = 0;
+    }
+    return match == len2 ? index : -1;
+  }
 };
 // @lc code=end
-
