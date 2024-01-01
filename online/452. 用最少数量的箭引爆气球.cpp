04b0b3a1aa9b507d52d7b448e506965111de73d1
comoco-5672 <775324623@qq.com>
2024-01-01 13:36:19 +0800

    添加最少数量的箭引爆气球

--- online/452. 用最少数量的箭引爆气球.cpp
+++ online/452. 用最少数量的箭引爆气球.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+// 贪心算法。首先按照气球的结束坐标进行升序排序，然后从左到右扫描，
+// 维护一个当前箭所能达到的最远结束坐标，如果下一个气球的开始坐标在当前箭的最远结束坐标之后，说明需要一支新的箭。
+
 int findMinArrowShots(vector<vector<int>> &points) {
   int len1 = points.size();
   sort(points.begin(), points.end(),
@@ -19,4 +22,14 @@ int findMinArrowShots(vector<vector<int>> &points) {
   return len1 - removed;
 }
 
-int main() { return 0; }
+int main() {
+  vector<vector<int>> points1 = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+  cout << findMinArrowShots(points1) << endl; // 输出 2
+
+  vector<vector<int>> points2 = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+  cout << findMinArrowShots(points2) << endl; // 输出 4
+
+  vector<vector<int>> points3 = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+  cout << findMinArrowShots(points3) << endl; // 输出 2
+  return 0;
+}
