4c96fa93168cac941b92ce26a997ee5d8d59cf2d
comoco-5672 <775324623@qq.com>
2024-01-03 22:20:13 +0800

    添加二叉树的最大路径和

---
+++ online/124. 二叉树中的最大路径和.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+int maxPathSum(TreeNode *root, int &maxSum) {
+  if (!root) {
+    return 0;
+  }
+
+  int leftSum = max(0, maxPathSum(root->left, maxSum));
+  int rightSum = max(0, maxPathSum(root->right, maxSum));
+
+  int currentPathSum = leftSum + rightSum + root->val;
+
+  maxSum = max(maxSum, currentPathSum);
+
+  return max(leftSum, rightSum) + root->val;
+}
+
+int maxPathSum(TreeNode *root) {
+  int maxSum = INT_MIN;
+  maxPathSum(root, maxSum);
+  return maxSum;
+}
+
+int main() {
+  TreeNode *root = new TreeNode(10);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(10);
+  root->left->left = new TreeNode(20);
+  root->left->right = new TreeNode(1);
+  root->right->right = new TreeNode(-25);
+  root->right->right->left = new TreeNode(3);
+  root->right->right->right = new TreeNode(4);
+
+  int result = maxPathSum(root);
+  cout << "Maximum path sum in the binary tree: " << result << endl;
+  return 0;
+}
