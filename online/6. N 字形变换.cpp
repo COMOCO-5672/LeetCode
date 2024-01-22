a81aa7f48ec7f55fd22c0cdfba2b3ce81e00bc75
comoco-5672 <775324623@qq.com>
2024-01-22 23:23:23 +0800

    添加整数翻转和N字变换

--- online/6. N 字形变换.cpp
+++ online/6. N 字形变换.cpp
@@ -5,37 +5,40 @@
 
 using namespace std;
 
-std::string convert(std::string s, int numRows) {
-  if (numRows == 1 || numRows >= s.size()) {
-    return s;
-  }
-
-  std::vector<std::string> rows(numRows);
-
-  int curRow = 0;
-  bool goingDown = false;
-
-  for (char c : s) {
-    rows[curRow] += c;
-    if (curRow == 0 || curRow == numRows - 1) {
-      goingDown = !goingDown;
+std::string convert(std::string s, int numRows)
+{
+    if (numRows == 1 || numRows >= s.size()) {
+        return s;
     }
-    curRow += goingDown ? 1 : -1;
-  }
 
-  std::string result;
-  for (const std::string &row : rows) {
-    result += row;
-  }
+    std::vector <std::string> rows(numRows);
 
-  return result;
+    int curRow = 0;
+    bool goingDown = false;
+
+    for (char c: s) {
+        rows[curRow] += c;
+        // 当位于第一行或者最后一行，需要掉头进行排列
+        if (curRow == 0 || curRow == numRows - 1) {
+            goingDown = !goingDown;
+        }
+        curRow += goingDown ? 1 : -1;
+    }
+
+    std::string result;
+    for (const std::string &row: rows) {
+        result += row;
+    }
+
+    return result;
 }
 
-int main() {
-  string input = "PAYPALISHIRING";
-  int numRows = 3;
-  string result = convert(input, numRows);
+int main()
+{
+    string input = "PAYPALISHIRING";
+    int numRows = 3;
+    string result = convert(input, numRows);
 
-  cout << "Converted string: " << result << endl;
-  return 0;
+    cout << "Converted string: " << result << endl;
+    return 0;
 }
--- online/7.整数反转.cpp
+++ online/7.整数反转.cpp
@@ -5,10 +5,29 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int reverse(int x) {
+    int reverse(int x)
+    {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
 
+            // INT_MAX 最大值是9223372036854775807
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && pop > 7)) {
+                return 0;
+            }
+
+            // INT_MIN 最小值是-9223372036854775808
+            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && pop < -8)) {
+                return 0;
+            }
+
+            result = result * 10 + pop;
+        }
+        return result;
     }
 };
 // @lc code=end
