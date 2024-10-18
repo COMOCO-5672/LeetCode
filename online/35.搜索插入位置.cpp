2f778efc76f6c4c2e8d0c6f490b2ef6871ce058d
comoco-5672 <775324623@qq.com>
2024-10-18 15:52:36 +0800

    调整代码

--- online/35.搜索插入位置.cpp
+++ online/35.搜索插入位置.cpp
@@ -14,6 +14,12 @@
 class Solution {
 public:
   int searchInsert(std::vector<int> &nums, int target) {
+
+#if 0
+// 具体来说，你的代码使用了 (low + high) / 2 来计算中间位置，而不是 low + (high - low) / 2。虽然这在大多数情况下不会造成问题，但在处理大数组时可能会导致整数溢出。
+
+// 另外，你的循环条件是 while (low <= high)，这是正确的做法。但在循环结束后，你使用了 return low++，这可能会导致返回值比实际插入位置大1。
+
     int low = 0;
     int high = nums.size() - 1;
     int mid = (low + high) / 2;
@@ -29,6 +35,23 @@ public:
       mid = (low + high) / 2;
     }
     return low++;
+#endif
+
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return left;
   }
 };
 // @lc code=end
--- online/66.加一.cpp
+++ online/66.加一.cpp
@@ -9,14 +9,39 @@
 
 class Solution {
 public:
-    std::vector<int> plusOne(std::vector<int>& digits) {
-        int length = digits.size();
-        for (int i = length - 1; i >= 0; i--)
-        {
-            
+  std::vector<int> plusOne(std::vector<int> &digits) {
+#if 0
+    int length = digits.size();
+    int flow = 1;
+    for (int i = length - 1; i >= 0; i--) {
+      if (flow) {
+        if (digits[i] + 1 > 9) {
+          digits[i] = (digits[i] + 1) % 10;
+          flow = 1;
+          if (i == 0) {
+            digits.insert(digits.begin(), 1);
+            break;
+          }
+        } else {
+          digits[i] += 1;
+          break;
         }
-        
+      } else {
+        break;
+      }
     }
+    return digits;
+#endif
+
+    for (int i = digits.size() - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    digits.insert(digits.begin(), 1);
+    return digits;
+  }
 };
 // @lc code=end
-
