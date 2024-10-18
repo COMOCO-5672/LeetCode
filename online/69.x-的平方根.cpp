964fd1b8667a0ea5d365d728a447f315c443814d
comoco-5672 <775324623@qq.com>
2024-10-18 16:19:36 +0800

    添加x的平方根计算

--- online/69.x-的平方根.cpp
+++ online/69.x-的平方根.cpp
@@ -1,23 +1,29 @@
 /*
  * @lc app=leetcode.cn id=69 lang=cpp
  *
- * [69] x 的平方根 
+ * [69] x 的平方根
  */
 
 // @lc code=start
 class Solution {
 public:
-    int mySqrt(int x) {
-        int l = 1, r = x, mid, sqrt;
+  int mySqrt(int x) {
 
-        while (l<=r)
-        {
-            mid = l + (r - l) / 2;
-        }
-        
-        
-
+    if (x == 0)
+      return x;
+    int l = 1, r = x, mid, sqrt;
+    while (l <= r) {
+      mid = l + (r - l) / 2;
+      sqrt = x / mid;
+      if (sqrt == mid) {
+        return mid;
+      } else if (mid > sqrt) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
     }
+    return r;
+  }
 };
 // @lc code=end
-
