99fd77ae7a6fbafb5865bebc9401d61b2f7ff8c1
comoco-5672 <775324623@qq.com>
2024-10-21 16:14:25 +0800

    添加括号生成

--- online/22. 括号生成.cpp
+++ online/22. 括号生成.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 class Solution
 {
 private:
@@ -9,6 +11,36 @@ private:
 public:
     Solution(/* args */);
     ~Solution();
+
+    vector<string> result;
+
+    void backtrack(string &current, int open, int close, int n)
+    {
+        if (current.length() == n * 2)
+        {
+            result.push_back(current);
+            return;
+        }
+
+        if (open < n) {
+            current.push_back('(');
+            backtrack(current, open + 1, close, n);
+            current.pop_back();
+        }
+
+        if (close < open)
+        {
+            current.push_back(')');
+            backtrack(current, open, close + 1, n);
+            current.pop_back();
+        }
+    }
+
+     vector<string> generateParenthesis(int n) {
+        string current;
+        backtrack(current, 0, 0, n);
+        return result;
+    }
 
     void dfs(std::vector<std::string> &ans, std::string cur, int left, int right, int n)
     {
